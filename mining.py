9fa2d4d01961bfd04ebbe4210a3dd160551768c9
arshanaqdi <arshanaqdi@gmail.com>
2019-03-08 11:22:07 +0330

    fix same issue

--- mining.py
+++ mining.py
@@ -2,12 +2,13 @@ import os
 import time
 from random import randint
 import random
-
+import sys
 
 os.system("clear")
 
 listName = []
 name = ""
+temp = 0
 listSeed = ["tor", "an", "rap", "ri", "kal", "cal", "oo",
             "z", "x", "ch", "rea", "ct", "hoo", "sh", "x", "ian",
             "on", "si", "dra", "lla", "ve", "pha", "tiny",
@@ -24,8 +25,7 @@ listSeed = ["tor", "an", "rap", "ri", "kal", "cal", "oo",
             "nus", "ora", "nep", "ton", "ry", "ars", "ju", "ura",
             "py", "tal", "cry", "bng", "ta", "the", "at", "we", "thy", "you", "me", "im",
             "am", "is", "are", "ger", "les", "ess", "lee", "gy", "er", "nol", "nil",
-            "so", "ti", "tan", "omi", "cy", "nul", "nl", "xi",
-            "ko", "qx", "xq", "ir", "ok", "dar", "sam"]
+            "so", "ti", "tan", "omi", "cy", "nul", "nl", "ir", "ok", "io"]
 
 
 f = open("list.txt", "r")
@@ -54,20 +54,28 @@ while True:
         continue
 
     listName.append(name)
-    with open("list.txt", "a") as text_file:
-        text_file.write(f"{name}, ")
+    if temp < 10:
+        temp += 1
+    else:
+        temp = 0
+        with open("list.txt", "a") as text_file:
+            text_file.write(f"{name}, ")
+        result = input("extract more ? (n -> no) -->> ")
+
+        if result.count("n"):
+            sys.exit()
+        else:
+            os.system(f"tput clear")
 
     max = randint(1, 100)
-    for progress in range(0, max, (100 / max).__int__()):
+    for progress in range(0, 100, (100 / max).__int__()):
         os.system("tput sc")
-        print(f" - - >> {colors.BOLD}{colors.FAIL}{colors.UNDERLINE}{progress}{colors.ENDC} \
-              {colors.OKBLUE}...{colors.ENDC}")
+        print(f" - - - >> {colors.BOLD}{colors.FAIL}{colors.UNDERLINE}{progress}{colors.ENDC} \
+            {colors.OKBLUE}do{colors.ENDC}")
         os.system("tput rc")
-        os.system("tput ed")
 
-    os.system("tput rc")
-    os.system("tput ed")
-    print(f"---->> {colors.BOLD}{colors.OKBLUE}{name}{colors.ENDC}")
+    print(f"---->> {colors.BOLD}{colors.OKBLUE}{name}{colors.ENDC} \
+          {colors.OKBLUE}done{colors.ENDC}")
     os.system(f"say {name}")
 
     name = ""
